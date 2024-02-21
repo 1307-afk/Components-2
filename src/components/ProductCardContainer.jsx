1431acf0f98e8a2b12c7bf819d22eef6f88eee85
nandini <nandini.email@example.com>
2024-02-20 19:33:35 -0500

    navbar and product card updated

--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,9 +1,10 @@
 import ProductCard from "./ProductCard";
 
-export default function ProductCardContainer() {
+export default function ProductCardContainer({products}) {
   return (
     <>
       <h1>Discover Your Favorites</h1>
+      {products.map(product)
       <ProductCard />
       <ProductCard />
       <ProductCard />
